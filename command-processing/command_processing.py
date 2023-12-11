dbdb2b26f7880cc5359dd65d394b628befe1eeca
Alex Zukowski <ziamor23@gmail.com>
2023-12-11 08:37:24 -0500

    Get a LLM service hooked up

--- command-processing/command_processing.py
+++ command-processing/command_processing.py
@@ -5,9 +5,9 @@ import numpy as np
 import speech_recognition as sr
 from vosk import Model, KaldiRecognizer
 import wave
-import sys
 import os
 import json
+import requests
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='gevent', path='/command-processing')
@@ -26,37 +26,34 @@ recognizer = KaldiRecognizer(model, RATE)
 recognizer.SetWords(True)
 recognizer.SetPartialWords(True)
 
-sys.stdout.flush()
-
 def process_audio_data(audio_data):
-    print("Processing audio data")
-    if recognizer.AcceptWaveform(audio_data.tobytes()):
-        result = json.loads(recognizer.Result())
-        text = result.get('text', '')
-        print("Transcribed text: ", text)
-        socketio.emit('command_detected', {'command': text})
-        return text
-    else:
-        # Handle partial results if needed
-        print("Partial result")
-        
-    sys.stdout.flush()
+    recognizer.AcceptWaveform(audio_data.tobytes())
 
+def send_text_to_llm(text):
+    print("Sending text to LLM service")
+    url = 'http://llm-processing:5003/process_text'
+    data = {'text': text + '\n'}
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        print("Text sent to LLM service successfully")
+        result = response.json()
+        print("LLM result: ", result)        
+        socketio.emit('command_detected', {'command': result['result']}) 
+    else:
+        print("Failed to send text to LLM service")
+        
 @socketio.on('connect')
 def handle_connect():
     print("Client connected")
-    sys.stdout.flush()
 
 @socketio.on('disconnect')
 def handle_disconnect():
     print("Client disconnected")
-    sys.stdout.flush()
 
 @socketio.on_error()
 def handle_error(e):
     print("An error occurred:")
     print(e)
-    sys.stdout.flush()
 
 @socketio.on('stream_audio')
 def handle_stream_audio(frame):
@@ -68,23 +65,15 @@ def handle_stream_audio(frame):
     else:
         print("Received non-bytes data")
         print(data)
-        
-    sys.stdout.flush()
 
 @socketio.on('process_command')
 def handle_process_command(data):
-	print("Received detect command event")
-	result = recognizer.FinalResult()
-	print("Final result: ", result)
-	sys.stdout.flush()
-	socketio.emit('command_detected', {'command': result})
-
-def process_wav_file(wav_file_path):
-    """Process a WAV file for testing."""
-    with wave.open(wav_file_path, 'rb') as wav_file:
-        frames = wav_file.readframes(wav_file.getnframes())
-        audio_data = np.frombuffer(frames, dtype=np.int16)
-        return process_audio_data(audio_data)
+    print("Received detect command event")
+    result = recognizer.FinalResult()
+    text = json.loads(result)['text']
+    print("Transcribed text: ", text)
+    print("Sending text to LLM service")
+    send_text_to_llm(text)   
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5002)
